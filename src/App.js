4f2410eba5e1d8964ab5865e888239349730ba71
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-11-06 10:43:03 +0300

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing, Error, Register, Dashboard } from "./pages";
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
