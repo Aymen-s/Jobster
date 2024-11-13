2aad326c503bb498a7d2fc25666faaee169a3d0f
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-11-13 16:35:01 +0300

    completing the navbar for both small and big screen

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,6 +1,28 @@
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 import Wrapper from "../assets/wrappers/BigSidebar";
-function BigSidebar() {
-  return <Wrapper>big sidebar</Wrapper>;
-}
+import { useSelector } from "react-redux";
+
+const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default BigSidebar;
