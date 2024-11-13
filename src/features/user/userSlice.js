7e919a4985e3b8e07e27d436b2e5de1c03030418
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-11-13 14:06:13 +0300

    completing register user functionality

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import customFetch from "../../utils/axios";
+import { toast } from "react-toastify";
 
 const initialState = {
   isLoading: false,
@@ -8,7 +10,12 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
-    console.log(`Register User : ${user}`);
+    try {
+      const resp = await customFetch.post("auth/register", user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
@@ -22,6 +29,22 @@ export const loginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        toast.success(`Welcome, ${user.name}!`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
